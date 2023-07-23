7453a32497580239ffd4476ccc4856f6489b125a
Jonatas <jonatas.07022003@gmail.com>
2023-07-23 14:15:39 -0300

    Setup do projeto e criação do header

---
+++ dist/index.c88557a6.js
@@ -0,0 +1,2 @@
+console.log("Ol\xe1 mundo!");
+//# sourceMappingURL=index.c88557a6.js.map
---
+++ dist/index.f75de5e1.js
@@ -0,0 +1,34 @@
+const menu = document.getElementById("menu");
+const menuClose = document.getElementById("menu-close");
+const links = document.getElementById("menu-links");
+const movimentoIR = [
+    {
+        transform: "translateX(300px)"
+    },
+    {
+        transform: "translateX(0px)"
+    }
+];
+const movimentoVOLTAR = [
+    {
+        transform: "translateX(0px)"
+    },
+    {
+        transform: "translateX(300px)"
+    }
+];
+const tempoEvento = {
+    duration: 200,
+    iterations: 1
+};
+menu.addEventListener("click", ()=>{
+    links.classList.add("menu--aparece");
+    links.animate(movimentoIR, tempoEvento);
+});
+menuClose.addEventListener("click", ()=>{
+    links.animate(movimentoVOLTAR, tempoEvento).addEventListener("finish", ()=>{
+        links.classList.remove("menu--aparece");
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
